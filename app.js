235524f1f4d44f396a347b36dadf8dc0c90acd80
sathish19961412 <sathishfreelanc5@gmail.com>
2023-02-02 10:34:35 +0530

    How to get data in string using stream and Buffer .Chunk data string in view in txt file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const fs =require('fs');
+const fs = require('fs');
+
 var server=http.createServer(function (req, res) {
    const url =req.url;
    const method =req.method;
@@ -8,12 +9,24 @@ var server=http.createServer(function (req, res) {
       res.setHeader('Content-type','text/html');
       res.write('<html>');
       res.write('<head><title>Enter User Details</title></head>');
-      res.write('<body><center><form action="/message" method="POST"><input type="text" name="username" /><input type="submit" name="value" /></form></body>');
+      res.write('<body><center><form  action="/message" method="POST"><input type="text" name="username"  /><input type="submit"/></form></body>');
       res.write('</html>');
       return res.end();   
    }
    if(url==='/message' && method=='POST')
    {
+      const body=[];
+      req.on('data',(chunk)=>{
+        body.push(chunk);
+        console.log(chunk);
+      })
+
+      req.on('end',()=>{
+         const parsedbody = Buffer.concat(body).toString();
+         const message=parsedbody.split('=');
+         fs.writeFileSync('message_split.txt',message[1]);
+      })
+
       fs.writeFileSync('hello.txt','Hello World');
       res.setHeader('Location','/');
       res.statusCode = 302;
